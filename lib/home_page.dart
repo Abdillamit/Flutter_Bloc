fd2ee8a0ba20f781ae52f9a91089a9c8fade957b
Abdillamit <abdillamit08@gmail.com>
2023-12-07 17:33:20 +0600

    done cubit

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,61 +1,52 @@
 import 'package:counter_blc/cubit/counter_cubit.dart';
+import 'package:counter_blc/inc_dnc_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-class MyHomePage extends StatefulWidget {
+class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key, required this.title});
-
+  static Page<void> page() => const MaterialPage<void>(
+          child: MyHomePage(
+        title: 'Flutter Demo Home Page',
+      ));
   final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  final counterCubit = CounterCubit();
   @override
   Widget build(BuildContext context) {
+    final counterCubit = BlocProvider.of<CounterCubit>(context);
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-          title: Text(widget.title),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              const Text(
-                'You have pushed the button this many times:',
-              ),
-              BlocBuilder<CounterCubit, int>(
-                bloc: counterCubit,
-                builder: (context, counter) {
-                  return Text(
-                    '$counter',
-                    style: Theme.of(context).textTheme.headlineMedium,
-                  );
-                },
-              )
-            ],
-          ),
-        ),
-        floatingActionButton: Column(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-            FloatingActionButton(
-              onPressed: () => counterCubit.incriment(),
-              tooltip: 'Increment',
-              child: const Icon(Icons.add),
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
             ),
-            const SizedBox(
-              height: 15,
-            ),
-            FloatingActionButton(
-              onPressed: () => counterCubit.decrement(),
-              tooltip: 'Decrement',
-              child: const Icon(Icons.minimize),
-            ),
+            BlocBuilder<CounterCubit, int>(
+              bloc: counterCubit,
+              builder: (context, counter) {
+                return Text(
+                  '$counter',
+                  style: Theme.of(context).textTheme.headlineMedium,
+                );
+              },
+            )
           ],
-        ));
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        heroTag: null,
+        onPressed: () {
+          Navigator.of(context).push(MaterialPageRoute(
+            builder: (context) => const IncDecPage(),
+          ));
+          print("NextPage");
+        },
+        child: const Icon(Icons.navigate_next),
+      ),
+    );
   }
 }
---
+++ lib/inc_dnc_page.dart
@@ -0,0 +1,49 @@
+import "package:counter_blc/cubit/counter_cubit.dart";
+import "package:counter_blc/home_page.dart";
+import "package:flutter/material.dart";
+import "package:flutter_bloc/flutter_bloc.dart";
+
+class IncDecPage extends StatelessWidget {
+  const IncDecPage({super.key});
+
+  static Page<void> page() => const MaterialPage<void>(child: IncDecPage());
+
+  @override
+  Widget build(BuildContext context) {
+    final counterCubit = BlocProvider.of<CounterCubit>(context);
+
+    return Scaffold(
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            heroTag: null,
+            onPressed: () => counterCubit.incriment(),
+            tooltip: 'Increment',
+            child: const Icon(Icons.add),
+          ),
+          const SizedBox(
+            height: 15,
+          ),
+          FloatingActionButton(
+            heroTag: null,
+            onPressed: () => counterCubit.decrement(),
+            tooltip: 'Decrement',
+            child: const Icon(Icons.minimize),
+          ),
+          FloatingActionButton(
+            heroTag: null,
+            onPressed: () {
+              Navigator.of(context).push(MaterialPageRoute(
+                builder: (context) =>
+                    const MyHomePage(title: 'Flutter Demo Home Page'),
+              ));
+              print("NextPage");
+            },
+            child: const Icon(Icons.navigate_next),
+          ),
+        ],
+      ),
+    );
+  }
+}
